f7bdc203e091aca8517fa8e53eb28ff610e2550e
duong <doduong352002@gmail.com>
2023-11-15 20:43:27 +0700

    login_2.0

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,75 +1,186 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:management_file/components/my_button.dart';
-import 'package:management_file/components/my_textfield.dart';
-class LoginPage extends StatelessWidget{
-  LoginPage({super.key});
-  //text editing controller
-  final usernameController=TextEditingController();
-  final passwordController=TextEditingController();
-  //final user in method
-  void signUserIn(){}
-  @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        backgroundColor: Colors.grey[300],
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                const SizedBox(height: 50),
-                // logo
-                const Icon(
-                  Icons.lock,
-                  size: 100,
-                ),
-                const SizedBox(height: 50),
-                //welcome
-                Text(
-                  'Welcome',
-                  style: TextStyle(
-                    color: Colors.grey[700],
-                    fontSize: 20,
-                    ),
-                ),
-                const SizedBox(height: 10),
-                //username textfield
-                MyTextField(
-                  controller: usernameController,
-                  hintText: 'Username',
-                  obscureText: false,
-                ),
-                const SizedBox(height: 10),
-                //password textfield
-                MyTextField(
-                  controller: passwordController,
-                  hintText: 'Password',
-                  obscureText: true,
-                ),
-                //forgot password
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        "Forgot Password?",
-                        style: TextStyle(color: Colors.grey[600]),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 50),
-                //sign in button
-                MyButton(
-                  onTap: signUserIn,
-                ),
-                //or continue with
 
-                //register
-              ]
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool isVisible = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 115, 126, 229),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              child: Image.asset(
+                'lib/images/logo_kma.png',
+                height: 200,
+                width: 200,
+              ),
             ),
-          )
-        )
-      );
-    }
+            const SizedBox(height: 3),
+            //welcome
+            Text(
+              'Management files',
+              style: TextStyle(
+                color: const Color.fromARGB(255, 227, 10, 10),
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 32),
+                constraints: BoxConstraints.expand(),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(35),
+                        topRight: Radius.circular(35))),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Text(
+                      'Sign In',
+                      style:
+                        TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    TextField(
+                      controller: emailController,
+                      onChanged: (value) {
+                        setState(() {});
+                      },
+                      decoration: InputDecoration(
+                          // icon: Icon(Icons.mail),
+                          prefixIcon: Icon(Icons.mail),
+                          suffixIcon: emailController.text.isEmpty
+                              ? Text('')
+                              : GestureDetector(
+                                  onTap: () {
+                                    emailController.clear();
+                                  },
+                                  child: Icon(Icons.close)),
+                          hintText: 'example@mail.com',
+                          labelText: 'Username',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide:
+                                  BorderSide(color: Colors.red, width: 1))),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    TextField(
+                      obscureText: isVisible,
+                      controller: passwordController,
+                      onChanged: (value) {
+                        print(value);
+                      },
+                      //
+                      decoration: InputDecoration(
+                          // icon: Icon(Icons.mail),
+                          prefixIcon: Icon(Icons.lock),
+                          suffixIcon: GestureDetector(
+                              onTap: () {
+                                isVisible = !isVisible;
+                                setState(() {});
+                              },
+                              child: Icon(isVisible
+                                  ? Icons.visibility
+                                  : Icons.visibility_off)),
+                          hintText: 'type your password',
+                          labelText: 'Password',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide:
+                                  BorderSide(color: Colors.red, width: 1))),
+                    ),
+
+
+                    
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            "Forgot Password?",
+                            style: TextStyle(color: Colors.grey[600]),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+
+                    //pop up output
+                    ElevatedButton(
+                        onPressed: () {
+                          /*showDialog(
+                              context: context,
+                              builder: (context) {
+                                return SimpleDialog(
+                                  title: Text('Your submitted data '),
+                                  children: [
+                                    ListTile(
+                                      leading: Icon(Icons.mail),
+                                      title:
+                                          Text(emailController.text.toString()),
+                                    ),
+                                    ListTile(
+                                      leading: Icon(Icons.lock),
+                                      title: Text(
+                                          passwordController.text.toString()),
+                                    ),
+                                  ],
+                                );
+                              }
+                          );*/
+                        },
+
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 10),
+                          child: Text('Submit'),
+                        )
+                    ),
+
+
+                    
+                    BottomAppBar(
+                      color: Colors.transparent,
+                      child:Row(
+                        children: [
+                          Icon(Icons.contact_mail_sharp),
+                          Text('  contact me: kma@actvn.edu.vn'),
+                        ],
+                        ),
+                      elevation: 0,
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
